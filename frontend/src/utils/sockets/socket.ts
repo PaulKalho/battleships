37dc76e8b69f09029fd2a95b6c72aba431256c9c
PaulKalho <kalhornpaul@gmail.com>
2023-05-25 18:09:59 +0200

    Dockerize Backend

--- frontend/src/utils/sockets/socket.ts
+++ frontend/src/utils/sockets/socket.ts
@@ -1,6 +1,10 @@
 import { Anybody } from "next/font/google";
 import { io } from "socket.io-client";
 
-const network = io("http://localhost:3001");
+const network = io(
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3001"
+    : "http://battleships-backend:3001"
+);
 
 export default network;
