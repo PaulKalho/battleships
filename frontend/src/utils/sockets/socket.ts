72e2fdfacb4dce417b9ca9e4943d56599b60a17e
PaulKalho <kalhornpaul@gmail.com>
2023-06-02 20:25:16 +0200

    improvements

--- frontend/src/utils/sockets/socket.ts
+++ frontend/src/utils/sockets/socket.ts
@@ -4,7 +4,8 @@ import { io } from "socket.io-client";
 const network = io(
   process.env.NODE_ENV === "development"
     ? "http://localhost:3001"
-    : "https://todo-backend.kalhorn.io"
+    : "https://backend-battleships.kalhorn.io",
+  { secure: true }
 );
 
 export default network;
